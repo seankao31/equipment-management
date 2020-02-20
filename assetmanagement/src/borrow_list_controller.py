1f2d4b2e1877087b5ca861d88d043f7a417c52c1
Sean Kao <seankao31@gmail.com>
2020-02-20 23:14:19 +0800

    borrow list controller modify view via methods

--- assetmanagement/src/borrow_list_controller.py
+++ assetmanagement/src/borrow_list_controller.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtWidgets
 
 from assetmanagement.src.borrow_list_view import BorrowListView
 
@@ -11,11 +11,11 @@ class BorrowListController:
         self.view.comboBox_Borrower.currentIndexChanged \
             .connect(self.update_table)
         self.view.pushButton_ClearBorrower.clicked \
-            .connect(self.deselect_borrower)
+            .connect(self.view.deselect_borrower_combobox)
         self.view.comboBox_Asset.currentIndexChanged \
             .connect(self.update_table)
         self.view.pushButton_ClearAsset.clicked \
-            .connect(self.deselect_asset)
+            .connect(self.view.deselect_asset_combobox)
         self.view.radioButton_All.toggled \
             .connect(self.update_table)
         self.view.radioButton_Borrowing.toggled \
@@ -25,64 +25,59 @@ class BorrowListController:
         self.view.buttonBox.rejected \
             .connect(self.dialog.reject)
 
+        self.model.add_borrower.add_observer(
+            self.update_borrower_combobox,
+            pass_arguments=False
+        )
+        self.model.deactivate_borrower.add_observer(
+            self.update_borrower_combobox,
+            pass_arguments=False
+        )
+        self.model.add_asset.add_observer(
+            self.update_asset_combobox,
+            pass_arguments=False
+        )
+        self.model.remove_asset.add_observer(
+            self.update_asset_combobox,
+            pass_arguments=False
+        )
+        self.model.borrow_asset.add_observer(
+            self.update_table,
+            pass_arguments=False
+        )
+        self.model.return_asset.add_observer(
+            self.update_table,
+            pass_arguments=False
+        )
+
     def run(self):
+        self.reset()
         self.dialog.show()
-        self.set_model()
 
-    def set_model(self):
-        self.view.comboBox_Borrower.clear()
-        borrower_names = self.model.get_borrower_names(active_only=True)
-        self.view.comboBox_Borrower.addItems(borrower_names)
-        self.deselect_borrower()
-
-        self.view.comboBox_Asset.clear()
-        assets = self.model.get_assets(active_only=True)
-        asset_names = [asset[0] for asset in assets]
-        self.view.comboBox_Asset.addItems(asset_names)
-        self.deselect_asset()
-
+    def reset(self):
+        self.update_borrower_combobox()
+        self.update_asset_combobox()
+        self.view.deselect_borrower_combobox()
+        self.view.deselect_asset_combobox()
+        self.view.radioButton_All.setChecked(True)
         self.update_table()
 
-    def update_table(self):
-        if self.view.comboBox_Borrower.currentIndex() == -1:
-            borrower_name = None
-        else:
-            borrower_name = self.view.comboBox_Borrower.currentText()
-        if self.view.comboBox_Asset.currentIndex() == -1:
-            asset_name = None
-        else:
-            asset_name = self.view.comboBox_Asset.currentText()
-        active_only = False
-        overdue_only = False
-        if self.view.radioButton_Borrowing.isChecked():
-            active_only = True
-        if self.view.radioButton_Overdue.isChecked():
-            overdue_only = True
+    def update_borrower_combobox(self):
+        borrower_names = self.model.get_borrower_names(active_only=True)
+        self.view.update_borrower_combobox(borrower_names)
 
+    def update_asset_combobox(self):
+        assets = self.model.get_assets(active_only=True)
+        asset_names = [asset[0] for asset in assets]
+        self.view.update_asset_combobox(asset_names)
+
+    def update_table(self):
+        borrower_name, asset_name, active_only, overdue_only =\
+            self.view.get_update_table_arguments()
         loans = self.model.get_loans(
             borrower_name=borrower_name,
             asset_name=asset_name,
             active_only=active_only,
             overdue_only=overdue_only
         )
-        table = self.view.tableWidget
-        table.clearContents()
-        table.setRowCount(0)
-        for row, loan in enumerate(loans):
-            table.insertRow(row)
-            table.setItem(row, 0, \
-                QtWidgets.QTableWidgetItem(loan[0]))
-            table.setItem(row, 1, \
-                QtWidgets.QTableWidgetItem(loan[1]))
-            table.setItem(row, 2, \
-                QtWidgets.QTableWidgetItem(str(loan[2])))
-            table.setItem(row, 3, \
-                QtWidgets.QTableWidgetItem(loan[3].strftime("%Y-%m-%d")))
-            table.setItem(row, 4, \
-                QtWidgets.QTableWidgetItem('Yes' if loan[4] else 'No'))
-
-    def deselect_borrower(self):
-        self.view.comboBox_Borrower.setCurrentIndex(-1)
-
-    def deselect_asset(self):
-        self.view.comboBox_Asset.setCurrentIndex(-1)
+        self.view.update_table(loans)
--- assetmanagement/src/borrow_list_view.py
+++ assetmanagement/src/borrow_list_view.py
@@ -1,5 +1,57 @@
+from PyQt5 import QtWidgets
+
 from assetmanagement.src.borrow_list_dialog import Ui_BorrowListDialog
 
 class BorrowListView(Ui_BorrowListDialog):
     def __init__(self, parent):
         self.setupUi(parent)
+
+    def deselect_borrower_combobox(self):
+        self.comboBox_Borrower.setCurrentIndex(-1)
+
+    def deselect_asset_combobox(self):
+        self.comboBox_Asset.setCurrentIndex(-1)
+
+    def update_borrower_combobox(self, borrower_names):
+        self.comboBox_Borrower.clear()
+        self.comboBox_Borrower.addItems(borrower_names)
+        self.deselect_borrower_combobox()
+
+    def update_asset_combobox(self, asset_names):
+        self.comboBox_Asset.clear()
+        self.comboBox_Asset.addItems(asset_names)
+        self.deselect_borrower_combobox()
+
+    def get_update_table_arguments(self):
+        if self.comboBox_Borrower.currentIndex() == -1:
+            borrower_name = None
+        else:
+            borrower_name = self.comboBox_Borrower.currentText()
+        if self.comboBox_Asset.currentIndex() == -1:
+            asset_name = None
+        else:
+            asset_name = self.comboBox_Asset.currentText()
+        active_only = False
+        overdue_only = False
+        if self.radioButton_Borrowing.isChecked():
+            active_only = True
+        if self.radioButton_Overdue.isChecked():
+            overdue_only = True
+        return borrower_name, asset_name, active_only, overdue_only
+
+    def update_table(self, loans):
+        table = self.tableWidget
+        table.clearContents()
+        table.setRowCount(0)
+        for row, loan in enumerate(loans):
+            table.insertRow(row)
+            table.setItem(row, 0, \
+                QtWidgets.QTableWidgetItem(loan[0]))
+            table.setItem(row, 1, \
+                QtWidgets.QTableWidgetItem(loan[1]))
+            table.setItem(row, 2, \
+                QtWidgets.QTableWidgetItem(str(loan[2])))
+            table.setItem(row, 3, \
+                QtWidgets.QTableWidgetItem(loan[3].strftime("%Y-%m-%d")))
+            table.setItem(row, 4, \
+                QtWidgets.QTableWidgetItem('Yes' if loan[4] else 'No'))
