4cafab29a13df994e6ad223a4124860c61bffd77
Sean Kao <seankao31@gmail.com>
2020-02-21 02:20:03 +0800

    application name

--- assetmanagement/src/application.py
+++ assetmanagement/src/application.py
@@ -1,12 +1,15 @@
+import sys
+
 from PyQt5 import QtWidgets
 
 from assetmanagement.src.main_controller import MainController
 
 class Application:
     def __init__(self):
-        self.app = QtWidgets.QApplication([])
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.app.setApplicationName("Asset Management System")
         self.controller = MainController()
 
     def run(self):
         self.controller.run()
-        self.app.exec_()
+        sys.exit(self.app.exec_())
