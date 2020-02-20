0bf3b6b7afb59364dc98376bbab8bbce8e924294
Sean Kao <seankao31@gmail.com>
2020-02-21 01:23:25 +0800

    borrow controller modify view via methods

--- assetmanagement/src/borrow_controller.py
+++ assetmanagement/src/borrow_controller.py
@@ -12,47 +12,91 @@ class BorrowController:
         self.view = BorrowView(self.dialog)
         self.model = model
 
+        self.view.comboBox_Asset.currentIndexChanged \
+            .connect(self.update_spinbox_range)
         self.view.buttonBox.accepted \
             .connect(self.borrow_asset)
         self.view.buttonBox.rejected \
             .connect(self.dialog.reject)
 
-    def run(self):
-        self.dialog.show()
-        self.set_model()
+        self.model.add_borrower.add_observer(
+            self.update_borrower_combobox,
+            pass_arguments=False
+        )
+        self.model.deactivate_borrower.add_observer(
+            self.update_borrower_combobox,
+            pass_arguments=False
+        )
+        self.model.add_asset.add_observer(
+            self.update_asset_combobox,
+            pass_arguments=False
+        )
+        self.model.remove_asset.add_observer(
+            self.update_asset_combobox,
+            pass_arguments=False
+        )
+        self.model.borrow_asset.add_observer(
+            self.update_asset_combobox,
+            pass_arguments=False
+        )
+        self.model.return_asset.add_observer(
+            self.update_asset_combobox,
+            pass_arguments=False
+        )
 
-    def set_model(self):
-        self.view.comboBox_Borrower.clear()
-        self.view.comboBox_Asset.clear()
+    def run(self):
+        self.reset()
+        self.dialog.show()
+
+    def reset(self):
+        self.update_borrower_combobox()
+        self.update_asset_combobox()
+        self.update_spinbox_range()
+        self.view.deselect_borrower_combobox()
+        self.view.deselect_asset_combobox()
+        self.view.clear_quantity_spinbox()
+
+    def update_borrower_combobox(self):
         borrower_names = self.model.get_borrower_names(active_only=True)
-        self.view.comboBox_Borrower.addItems(borrower_names)
+        self.view.update_borrower_combobox(borrower_names)
+        self.view.deselect_borrower_combobox()
+
+    def update_asset_combobox(self):
         assets = self.model.get_assets(instock_only=True)
         asset_names = [asset[0] for asset in assets]
-        self.view.comboBox_Asset.addItems(asset_names)
+        self.view.update_asset_combobox(asset_names)
+        self.view.deselect_asset_combobox()
+
+    def update_spinbox_range(self):
+        _, asset_name, _ = self.view.get_borrow_arguments()
+        if asset_name is None:
+            self.view.set_quantity_spinbox_range(0, 0)
+        else:
+            asset = self.model.get_asset(asset_name)
+            # asset[1] is total, asset[2] is instock
+            self.view.set_quantity_spinbox_range(1, asset[2])
 
     def borrow_asset(self):
-        borrower_name = self.view.comboBox_Borrower.currentText()
-        asset_name = self.view.comboBox_Asset.currentText()
-        quantity = self.view.spinBox_Quantity.value()
+        borrower_name, asset_name, quantity = self.view.get_borrow_arguments()
         datedue = date.today() + timedelta(days=7)
         # combobox ensures that NoResultFound will not be raised
-        try:
-            self.model.borrow_asset(
-                borrower_name=borrower_name,
-                asset_name=asset_name,
-                quantity=quantity,
-                datedue=datedue
-            )
-            self.dialog.accept()
-        except ValueError:
-            # can be avoided by setting spinbox range
+        if borrower_name is None:
             error_message(
                 self.dialog,
-                'Quantity should be positive.'
+                'Please select a borrower.'
             )
-        except IntegrityError:
-            # can be avoided by setting combobox item
+            return
+        if asset_name is None:
             error_message(
                 self.dialog,
-                'Not enough asset <{}> to be borrowed.'.format(asset_name)
+                'Please select an asset.'
             )
+            return
+        # exceptions avoided by spinbox range setting
+        self.model.borrow_asset(
+            borrower_name=borrower_name,
+            asset_name=asset_name,
+            quantity=quantity,
+            datedue=datedue
+        )
+        self.dialog.accept()
--- assetmanagement/src/borrow_view.py
+++ assetmanagement/src/borrow_view.py
@@ -3,3 +3,36 @@ from assetmanagement.src.borrow_dialog import Ui_BorrowDialog
 class BorrowView(Ui_BorrowDialog):
     def __init__(self, parent):
         self.setupUi(parent)
+
+    def deselect_borrower_combobox(self):
+        self.comboBox_Borrower.setCurrentIndex(-1)
+
+    def deselect_asset_combobox(self):
+        self.comboBox_Asset.setCurrentIndex(-1)
+
+    def clear_quantity_spinbox(self):
+        self.spinBox_Quantity.setValue(self.spinBox_Quantity.minimum())
+
+    def update_borrower_combobox(self, borrower_names):
+        self.comboBox_Borrower.clear()
+        self.comboBox_Borrower.addItems(borrower_names)
+
+    def update_asset_combobox(self, asset_names):
+        self.comboBox_Asset.clear()
+        self.comboBox_Asset.addItems(asset_names)
+
+    def get_borrow_arguments(self):
+        if self.comboBox_Borrower.currentIndex() == -1:
+            borrower_name = None
+        else:
+            borrower_name = self.comboBox_Borrower.currentText()
+        if self.comboBox_Asset.currentIndex() == -1:
+            asset_name = None
+        else:
+            asset_name = self.comboBox_Asset.currentText()
+        quantity = self.spinBox_Quantity.value()
+        return borrower_name, asset_name, quantity
+
+    def set_quantity_spinbox_range(self, minimum, maximum):
+        self.spinBox_Quantity.setMinimum(minimum)
+        self.spinBox_Quantity.setMaximum(maximum)
